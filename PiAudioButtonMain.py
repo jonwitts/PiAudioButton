858dfc7afd15bd323cc0782a3b060be529c9d5c4
jonwitts <jonwitts@gmail.com>
2019-03-20 21:48:58 +0000

    add button detection

--- PiAudioButtonMain.py
+++ PiAudioButtonMain.py
@@ -3,8 +3,6 @@
 import pygame
 from time import sleep
 from random import choice
-#from os import system
-#from signal import pause
 from gpiozero import Button
 
 audio_btn = Button(17)
@@ -16,24 +14,7 @@ bg.play(loops=-1) # loops indefinitely
 random_audio = ['./audio_files/random1.wav', './audio_files/random2.wav', './audio_files/random3.wav', './audio_files/random4.wav']
 
 while True:
-    ranAud = pygame.mixer.Sound(choice(random_audio))
-    ranAud.play()
-    sleep(2.0)
-
-#def shutdownAudio():
-    # stop all active mixer sounds from playing
- #   pygame.mixer.stop()
-    
-    # load our shutdown sound
-    
-    # and play it
-  #  s.play()
-    # loop whilst mixer is busy
-   # while pygame.mixer.get_busy():
-    #    sleep(0.05)
-    
-    # sound has stoped now shutdown
-    #system("sudo shutdown now -hP")
-
-#shutdown_btn.when_held = shutdownAudio
-#pause()
+    if audio_btn.is_pressed:
+        ranAud = pygame.mixer.Sound(choice(random_audio))
+        ranAud.play()
+        sleep(2)
