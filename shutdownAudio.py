a58f681b4aa215c2ecaad56f25a5013921203dfd
jonwitts <jonwitts@gmail.com>
2019-03-20 21:21:20 +0000

    complete shutdown file

--- shutdownAudio.py
+++ shutdownAudio.py
@@ -3,21 +3,26 @@
 import pygame
 from time import sleep
 from os import system
+from signal import pause
+from gpiozero import Button
 
+shutdown_btn = Button(21, hold_time=4)
 pygame.init()
 
-# stop all active mixer sounds from playing
-pygame.mixer.stop()
+def shutdownAudio():
+    # stop all active mixer sounds from playing
+    pygame.mixer.stop()
+    
+    # load our shutdown sound
+    s = pygame.mixer.Sound("./audio_files/shutdown-sound.wav")
+    # and play it
+    s.play()
+    # loop whilst mixer is busy
+    while pygame.mixer.get_busy():
+        sleep(0.05)
+    
+    # sound has stoped now shutdown
+    system("sudo shutdown now -hP")
 
-# load our shutdown sound
-s = pygame.mixer.Sound("./audio_files/shutdown-sound.wav")
-# and play it
-s.play()
-# loop whilst mixer is busy
-while pygame.mixer.get_busy():
-    #print("Mixer is busy")
-    sleep(0.05)
-
-# sound has stoped now shutdown
-#print("shutting down now")
-system("sudo shutdown now -hP")
+shutdown_btn.when_held = shutdown
+pause()
