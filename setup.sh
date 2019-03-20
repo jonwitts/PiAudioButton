46807657910505e7897ed01d78fb9f5b9731cca5
jonwitts <jonwitts@gmail.com>
2019-03-20 22:07:37 +0000

    add service files and adjust setup

--- setup.sh
+++ setup.sh
@@ -18,9 +18,31 @@ apt-get dist-upgrade -y
 # install our required packages
 echo "Installing dependencies..."
 echo "=========================="
-apt-get install wget git python3 -y
+apt-get install git python3 -y
 apt-get install python3-gpiozero python3-pygame -y
 
-# clone our repo from GitHub
-cd ~
+# clone our repo from GitHub and move into directory
+cd /
 git clone https://github.com/jonwitts/PiAudioButton.git
+cd /piAudioButton
+
+# copy and activate our systemd definitions
+echo "Copy and activate our systemd definitions..."
+echo "=========================="
+# PiAudioButton service
+cp ./PiAudioButton.service /lib/systemd/system/PiAudioButton.service
+chmod 644 /lib/systemd/system/PiAudioButton.service
+
+# shutdownAudio service
+cp ./shutdownAudio.service /lib/systemd/system/shutdownAudio.service
+chmod 644 /lib/systemd/system/shutdownAudio.service
+
+# reload and enable
+systemctl daemon-reload
+systemctl enable PiAudioButton.service
+systemctl enable shutdownAudio.service
+
+# done
+echo "Done. Rebooting now"
+echo "=========================="
+reboot
