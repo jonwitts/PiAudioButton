4001f3d9515ce722ab7ebff94e017815b82ed8fa
jonwitts <jonwitts@gmail.com>
2019-03-21 10:39:10 +0000

    use rc.local rather than systemd

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ apt-get dist-upgrade -y
 # install our required packages
 echo "Installing dependencies..."
 echo "=========================="
-apt-get install git python3 omxplayer -y
+apt-get install git python3 -y
 apt-get install python3-gpiozero python3-pygame -y
 
 # clone our repo from GitHub and move into directory
@@ -27,20 +27,25 @@ git clone https://github.com/jonwitts/PiAudioButton.git
 cd /PiAudioButton
 
 # copy and activate our systemd definitions
-echo "Copy and activate our systemd definitions..."
-echo "=========================="
+#echo "Copy and activate our systemd definitions..."
+#echo "=========================="
 # PiAudioButton service
-cp ./PiAudioButton.service /lib/systemd/system/PiAudioButton.service
-chmod 644 /lib/systemd/system/PiAudioButton.service
+#cp ./PiAudioButton.service /lib/systemd/system/PiAudioButton.service
+#chmod 644 /lib/systemd/system/PiAudioButton.service
 
 # shutdownAudio service
-cp ./shutdownAudio.service /lib/systemd/system/shutdownAudio.service
-chmod 644 /lib/systemd/system/shutdownAudio.service
+#cp ./shutdownAudio.service /lib/systemd/system/shutdownAudio.service
+#chmod 644 /lib/systemd/system/shutdownAudio.service
 
 # reload and enable
-systemctl daemon-reload
-systemctl enable PiAudioButton.service
-systemctl enable shutdownAudio.service
+#systemctl daemon-reload
+#systemctl enable PiAudioButton.service
+#systemctl enable shutdownAudio.service
+
+# Add python scripts to rc.local
+echo "Add Python scripts to rc.local"
+echo "=========================="
+sed -i -e '$i sudo /PiAudioButton/PiAudioButtonMain.py &\nsudo /PiAudioButton/shutdownAudio.py &\n' /etc/rc.local
 
 # done
 echo "Done. Rebooting now"
