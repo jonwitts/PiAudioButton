f62880c6211d10bbb900d86e8acf03f267af574f
jonwitts <jonwitts@gmail.com>
2019-03-21 11:27:49 +0000

    remove separate shutdown files

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Setup script to install our required software and 
-# configure services etc.
+# configure start up etc.
 
 # Make sure script is run as root.
 if [ "$(id -u)" != "0" ]; then
@@ -21,16 +21,16 @@ echo "=========================="
 apt-get install git python3 -y
 apt-get install python3-gpiozero python3-pygame -y
 
-# clone our repo from GitHub and move into directory
+# clone our repo from GitHub
 cd /
 git clone https://github.com/jonwitts/PiAudioButton.git
 
-# Add python scripts to rc.local
-echo "Add Python scripts to rc.local"
+# Add python script to rc.local
+echo "Add Python script to rc.local"
 echo "=========================="
 sed -i -e '$i sudo /PiAudioButton/PiAudioButtonMain.py &\n' /etc/rc.local
 
 # done
 echo "Done. Rebooting now"
 echo "=========================="
-#reboot
+reboot
